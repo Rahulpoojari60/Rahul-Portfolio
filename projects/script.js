24e19ecafb27aa1fce6fa8a4713662625e49d986
Rahul <rahulpoojari583@gmail.com>
2024-08-31 14:36:22 +0530

    first commit

--- projects/script.js
+++ projects/script.js
@@ -106,7 +106,15 @@ getProjects().then(data => {
 // fetch projects end
 
 // Start of Tawk.to Live Chat
-
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66d2dbaa50c10f7a00a266e7/1i6jqk4st';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 
 // End of Tawk.to Live Chat
 
